af86dbe578c2650a91e8cfd6beec532f26ee94e8
Darlan <dcpoffo@gmail.com>
2020-04-01 22:54:09 -0300

    primeiro commit

---
+++ ExeFixacao/Entities/Order.cs
@@ -0,0 +1,56 @@
+﻿using ExeFixacao.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExeFixacao.Entities
+{
+    class Order
+    {
+        public DateTime Date { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Itens { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime date, OrderStatus orderStatus, Client client)
+        {
+            Date = date;
+            OrderStatus = orderStatus;
+            Client = client;
+        }
+
+        public void AddItem (OrderItem item)
+        {
+            Itens.Add(item);
+        }
+
+        public void RemoveItem (OrderItem item)
+        {
+            Itens.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0.0;
+            // para cada ItemPedido (apelidado de item) na lista de ItensPedido
+            foreach (OrderItem item in Itens)
+            {
+                sum += item.SubTotal();
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            // fazer construtor do ToString
+            // para apresentar o sumario do pedido
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ ExeFixacao/Program.cs
@@ -0,0 +1,29 @@
+﻿using ExeFixacao.Entities;
+using ExeFixacao.Entities.Enums;
+using System;
+
+namespace ExeFixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENTER CLIENT DATA:");
+            Console.Write("Name: ");
+            string clientName = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("*******************");
+            Console.WriteLine("ENTER ORDER DATA: ");
+            Console.Write("Status (PendingPayment/Processing/Shipped/Delivered: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            //instanciando um novo cliente
+            Client client = new Client(clientName, email, birthDate);
+            //instanciando um novo pedido
+            Order order = new Order(DateTime.Now, status, client);
+        }
+    }
+}
